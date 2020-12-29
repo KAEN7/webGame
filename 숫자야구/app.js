76796ecef9affc9dc1dc1c3e3d6c45f015abb1e5
kaen <kusdsuna@naver.com>
2020-12-30 02:06:31 +0900

    Add else 홈런 for 숫자야구

--- 숫자야구/app.js
+++ 숫자야구/app.js
@@ -1,9 +1,8 @@
 // SELECTOR
-const result = document.querySelector('.resultJs');
+const resultHeader = document.querySelector('.resultJs');
 const input = document.querySelector('.inputJs');
 const btn = document.querySelector('.inputBtn');
 
-// LET
 let numbers = [1,2,3,4,5,6,7,8,9];
 
 let numberArray = [];
@@ -28,12 +27,28 @@ function maxLengthCheck(object) {
 function testNumber(event) {
     event.preventDefault();
     const answer = input.value;
-    if(numberArray.join('') === answer) {
-        result.innerText = '홈런!!!'
-    } else if(numberArray[i] === answer[i]) {
-        if() {
-            
+    if(numberArray.join('') === answer) { // 홈런일때
+        resultHeader.innerText = '홈런!!!'
+        input.value = '';
+        input.focus();
+        let numbers = [1,2,3,4,5,6,7,8,9];
+        let numberArray = [];
+        for (let i = 0; i < 4; i += 1) {
+            let result = numbers.splice(Math.floor(Math.random() * (9 -i)), 1)[0];
+            numberArray.push(result);
         }
+    } else { // 홈런이 아닐때
+        let resultArray = answer.split(''); // split는 string을 여러개의 문자열로 나눔
+        let strike = 0;
+        let ball = 0;
+        for(let i = 0; i < 3; i++) {
+            if(Number(resultArray[i]) === numberArray[i]) {
+                strike += 1;
+            } else if(numberArray.indexOf(Number(resultArray[i])) > -1) { // 배열.indexOf(값) 하면 값의 위치를 알 수 있음, 없으면 -1
+                ball += 1;
+            }
+        }
+        resultHeader.innerText = `${strike}S ${ball}B`;
     }
 }
 
