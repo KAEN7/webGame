e5e8e76d166a32cdd68c377bebe66ecdb00776ae
kaen <kusdsuna@naver.com>
2020-12-30 03:28:13 +0900

    Refactoring for 숫자야구

--- 숫자야구/app.js
+++ 숫자야구/app.js
@@ -3,21 +3,34 @@ const resultHeader = document.querySelector('.resultJs');
 const input = document.querySelector('.inputJs');
 const btn = document.querySelector('.inputBtn');
 
-let numbers = [1,2,3,4,5,6,7,8,9];
-
-let numberArray = [];
-
-for (let i = 0; i < 4; i += 1) {
-    let result = numbers.splice(Math.floor(Math.random() * (9 -i)), 1)[0];
-    numberArray.push(result);
-}
-
-console.log(numberArray.join(''));
-
 // 배열.join('') -> 문자
 // 문자.split('') -> 배열
 
+//LET
+let numbers;
+
+let numberArray;
+
 // FUNCTION
+function randomNumber() {
+    numbers = [1,2,3,4,5,6,7,8,9];
+
+    numberArray = [];
+
+    for (let i = 0; i < 4; i += 1) {
+        let result = numbers.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        numberArray.push(result);
+    }
+
+    console.log(numberArray.join(''));
+}
+randomNumber();
+
+function resetInput() {
+    input.value = '';
+    input.focus();
+}
+
 function maxLengthCheck(object) {
     if(object.value.length > object.maxLength) {
         object.value = object.value.slice(0, object.maxLength);
@@ -29,14 +42,8 @@ function testNumber(event) {
     const answer = input.value;
     if(numberArray.join('') === answer) { // 홈런일때
         resultHeader.innerText = '홈런!!!'
-        input.value = '';
-        input.focus();
-        let numbers = [1,2,3,4,5,6,7,8,9];
-        let numberArray = [];
-        for (let i = 0; i < 4; i += 1) {
-            let result = numbers.splice(Math.floor(Math.random() * (9 -i)), 1)[0];
-            numberArray.push(result);
-        }
+        resetInput();
+        randomNumber();
     } else { // 홈런이 아닐때
         let resultArray = answer.split(''); // split는 string을 여러개의 문자열로 나눔
         let strike = 0;
@@ -48,7 +55,13 @@ function testNumber(event) {
                 ball += 1;
             }
         }
+        input.value = '';
+        input.focus();
         resultHeader.innerText = `${strike}S ${ball}B`;
+        const out = `${0}S ${0}B`;
+        if(out === resultHeader.innerText) {
+            resultHeader.innerText = 'OUT!';
+        }
     }
 }
 
