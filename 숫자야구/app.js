b2ff252f1b70aae5726532c5cbc758576e5e9a85
kaen <kusdsuna@naver.com>
2020-12-29 22:59:32 +0900

    Add testNumber function for 숫자야구

--- 숫자야구/app.js
+++ 숫자야구/app.js
@@ -1,5 +1,9 @@
+// SELECTOR
+const result = document.querySelector('.resultJs');
 const input = document.querySelector('.inputJs');
+const btn = document.querySelector('.inputBtn');
 
+// LET
 let numbers = [1,2,3,4,5,6,7,8,9];
 
 let numberArray = [];
@@ -9,7 +13,31 @@ for (let i = 0; i < 4; i += 1) {
     numberArray.push(result);
 }
 
-console.log(numberArray);
+console.log(numberArray.join(''));
 
 // 배열.join('') -> 문자
 // 문자.split('') -> 배열
+
+// FUNCTION
+function maxLengthCheck(object) {
+    if(object.value.length > object.maxLength) {
+        object.value = object.value.slice(0, object.maxLength);
+    }
+}
+
+function testNumber(event) {
+    event.preventDefault();
+    const answer = input.value;
+    if(numberArray.join('') === answer) {
+        result.innerText = '홈런!!!'
+    } else if(numberArray[i] === answer[i]) {
+        if() {
+            
+        }
+    }
+}
+
+// if
+if(btn) {
+    btn.addEventListener('click', testNumber);
+}
