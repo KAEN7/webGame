c03d3736e7f9122f16ae04e93d86ca397610ff45
kaen notebook <kusdsuna@naver.com>
2021-01-02 17:51:27 +0900

    Add line check for 틱택토

--- 틱택토/app.js
+++ 틱택토/app.js
@@ -1,11 +1,15 @@
 // 클릭 => 번갈아가면서 'x' or 'o' => 한줄이 됬는지 반복 => 한줄이면 승자 확인 아니면 다시 클릭
+
+// CONST
 const td = document.querySelectorAll('td');
+const winLose = document.querySelector('h3');
 
 let turn = 'X';
 
 function clkTd(event) {
     const tdId = event.target.id;
     console.log(tdId);
+    // 턴 체크
     if(event.target.innerText = ' ') {
         event.target.innerText = turn;
         if(turn === 'O') {
@@ -14,7 +18,23 @@ function clkTd(event) {
             turn = 'O';
         }
     }
+    // 줄 체크
+    let winnerCheck = false;
+    console.log(td[0].innerText, td[4].innerText, td[8].innerText);
+    if(
+        td[0].innerText === turn && 
+        td[4].innerText === turn && 
+        td[8].innerText === turn
+        ) {
+        console.log('동작되면안됨');
+        winnerCheck = true;
+    }
+    
+    if(winnerCheck === true) {
+        winLose.innerText = 'WIN!!!'
+    }
 }
+
 
 for(i=0; i<9; i++) {
     td[i].addEventListener('click', clkTd);
