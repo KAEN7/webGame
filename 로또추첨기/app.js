a5c49dd0c21f334e5fb1723f809853cb37822ba8
kaen <kusdsuna@naver.com>
2021-01-06 13:24:43 +0900

    Update result value for 로또추첨기

--- 로또추첨기/app.js
+++ 로또추첨기/app.js
@@ -1,7 +1,34 @@
+const resultBar = document.querySelector('.resultBar');
+
+// 숫자 생성
 let numbers = [];
 
 for(let i=1; i<46; i++) {
     numbers.push(i);
 }
 
-console.log(numbers);
+let maping = numbers.map(function(i, index) {
+    return index + 1;
+});
+
+// 숫자 섞기
+let shuffle = [];
+
+while(maping.length>0) {
+    let moveValue = maping.splice(Math.floor(Math.random() * maping.length), 1)[0]; // 이동값
+    // Math.random은 실제로 완벽한 랜덤이 아님 ㅇㅋ?
+    shuffle.push(moveValue);
+}
+
+// 결과 값
+let bonus = shuffle[shuffle.length - 1];
+let lottoArray = shuffle.slice(0, 6);
+let lotto = lottoArray.sort((a,b) => a - b);
+
+for(i=0; i<lotto.length; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.innerText = lotto[i];
+    newDiv.classList = lotto[i];
+    resultBar.append(newDiv);
+
+}
